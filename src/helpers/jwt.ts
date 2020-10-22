806956cb52882ccfbd6fee37ed1dfd7160fe1aae
Tiago Boer Casagrande <tiago.boer@hotmail.com>
2020-10-21 22:10:09 -0300

    Formatação helpers

--- src/helpers/jwt.ts
+++ src/helpers/jwt.ts
@@ -1,5 +1,4 @@
 import jwt from 'jsonwebtoken';
-import {AuthChecker} from 'type-graphql';
 
 const options = {expiresIn: '30 minutes'};
 const privateKey = process.env.JWT_PRIVATE_KEY ?? '';
@@ -8,27 +7,29 @@ const verify = (token: string) => {
   return jwt.verify(token, privateKey);
 };
 
-export const generate = (payload: string | object) => {
-  return jwt.sign(payload, privateKey, options);
+const Auth = {
+  generate: (payload: string | object) => {
+    return jwt.sign(payload, privateKey, options);
+  },
+  mapContext: ({req}: any) => {
+    let user: any;
+    const token = req.headers.authorization;
+
+    if (typeof token === 'string') {
+      try {
+        const decoded = verify(token.slice(7, token.length));
+        user = decoded;
+      } catch (e) {}
+    }
+
+    return {user};
+  },
+  authChecker: ({context}: any) => {
+    const user = context.user;
+
+    if (!user) return false;
+    return true;
+  },
 };
 
-export const mapContext = ({req}: any) => {
-  let user: any;
-  const token = req.headers.authorization;
-
-  if (typeof token === 'string') {
-    try {
-      const decoded = verify(token.slice(7, token.length));
-      user = decoded;
-    } catch (e) {}
-  }
-
-  return {user};
-};
-
-export const authChecker: AuthChecker = ({context}: any) => {
-  const user = context.user;
-
-  if (!user) return false;
-  return true;
-};
+export default Auth;
